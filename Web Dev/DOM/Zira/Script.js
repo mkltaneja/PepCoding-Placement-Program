87df932698fab0546deb2063a145a177169e7b35
Mukul <mkltaneja@gmail.com>
2021-04-30 13:46:53 +0530

    Zira App

---
+++ Web Dev/DOM/Zira/Script.js
@@ -0,0 +1,60 @@
+let TC = document.querySelector(".ticket-container");
+let allFilters = document.querySelectorAll(".filter");
+let modalVisible = false;
+
+for (let i = 0; i < allFilters.length; i++) {
+    allFilters[i].addEventListener("click", filterHandler);
+    // console.log(allFilters[i]);
+}
+
+function filterHandler(e) {
+
+}
+
+let addButton = document.querySelector(".add");
+
+addButton.addEventListener("click", showModal);
+
+function showModal(e)
+{
+    if(!modalVisible)
+    {
+        modalVisible = true;
+        let modal = `<div class="modal">
+                        <div class="task-to-be-added" data-type="false" spellcheck="false" contenteditable="true">
+                            <span class="placeholder">Kuch Likhle</span>
+                        </div>
+                        <div class="priority-list">
+                            <div class="pink-modal-filter modal-filter active"></div>
+                            <div class="blue-modal-filter modal-filter"></div>
+                            <div class="yellow-modal-filter modal-filter"></div>
+                            <div class="green-modal-filter modal-filter"></div>
+                        </div>
+                    </div>`;
+        TC.innerHTML = TC.innerHTML + modal;
+        let taskTyper = document.querySelector(".task-to-be-added");
+        taskTyper.addEventListener("click", function(e){
+            if(e.currentTarget.getAttribute("data-type") == "false")
+            {
+                e.currentTarget.innerHTML = "";
+                e.currentTarget.setAttribute("data-type","true");
+            }
+        });
+        let modalFilters = document.querySelectorAll(".modal-filter");
+        for(let i = 0; i < modalFilters.length; i++)
+            modalFilters[i].addEventListener("click", selectPriority);
+    }
+}
+
+function selectPriority(e)
+{
+    let activeFilter = document.querySelector(".modal-filter.active");
+    activeFilter.classList.remove("active");
+    e.currentTarget.classList.add("active");
+}
+
+
+
+
+
+
