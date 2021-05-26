b305e02afd1111cbe6eb19ad2c9406897f81fd7f
Mukul <mkltaneja@gmail.com>
2021-05-26 13:54:59 +0530

    Coin Change Combinations

---
+++ IP List/Backtracking/27_Coin Change - Combinations - 1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void coin_change(int idx, int tar, int n, vector<int> &coins, string ans)
+{
+    if (tar == 0)
+    {
+        cout << ans + '.' << endl;
+        return;
+    }
+    for (int i = idx; i < n; i++)
+        if (tar - coins[i] >= 0)
+            coin_change(i + 1, tar - coins[i], n, coins, ans + to_string(coins[i]) + '-');
+}
+
+int main()
+{
+    int n, tar;
+    cin >> n;
+    vector<int> coins(n);
+    for (int i = 0; i < n; i++)
+        cin >> coins[i];
+    cin >> tar;
+    coin_change(0, tar, n, coins, "");
+}
