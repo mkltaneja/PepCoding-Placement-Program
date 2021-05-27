ee13e8fc1ff5ecbabb51571331ddefc9fcc62726
Mukul <mkltaneja@gmail.com>
2021-05-27 12:18:55 +0530

    K Swaps OPTIMIZED

--- IP List/Backtracking/36_Largest Number Possible After At Most K Swaps.cpp
+++ IP List/Backtracking/36_Largest Number Possible After At Most K Swaps.cpp
@@ -1,5 +1,5 @@
 
-// APPROACH 1 (Brute Force Backtracking)
+// APPROACH 1 (Brute Force Backtracking) --> O((n^2)^k)
 
 #include <iostream>
 #include <vector>
@@ -38,4 +38,45 @@ int main()
     cout << maxans << endl;
 }
 
+// APPROACH 2 (Sort the string in decreasing order, and check if current index doesn't match with the maximum char then swap) --> O(n^2) to O(n^n)
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string maxans = "";
+void kswaps(int i, int k, string &s, string &maxs)
+{
+    if (maxans.empty() || s > maxans)
+        maxans = s;
+    if (i == s.size() || k == 0)
+        return;
+
+    if (s[i] != maxs[i])
+    {
+        for (int j = i + 1; j < s.size(); j++)
+        {
+            if (s[j] == maxs[i])
+            {
+                swap(s[i], s[j]);
+                kswaps(i + 1, k - 1, s, maxs);
+                swap(s[i], s[j]);
+            }
+        }
+    }
+    else
+        kswaps(i + 1, k, s, maxs);
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+    int k;
+    cin >> k;
+    string ms = s;
+    sort(ms.begin(), ms.end(), greater<char>());
+    kswaps(0, k, s, ms);
+    cout << maxans << endl;
+}
