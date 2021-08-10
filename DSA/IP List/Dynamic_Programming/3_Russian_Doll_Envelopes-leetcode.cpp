88fb8248a7d32a2fbe15ced606b5da309ade8666
MUKUL <mkltaneja@gmail.com>
2021-08-10 22:27:33 +0530

    DP - 4

--- DSA/IP List/Dynamic_Programming/3_Russian_Doll_Envelopes-leetcode.cpp
+++ DSA/IP List/Dynamic_Programming/3_Russian_Doll_Envelopes-leetcode.cpp
@@ -1,1 +1,25 @@
-	
+
+int maxEnvelopes(vector<vector<int>>& envelopes) 
+{
+    int n = envelopes.size();
+    sort(envelopes.begin(), envelopes.end(), [](const auto &a, const auto &b){
+        return a[0] == b[0]? a[1] > b[1] : a[0] < b[0];
+    });
+    
+    vector<int> dp(n);
+    int maxlen = 0;
+    for(vector<int> v : envelopes)
+    {
+        int l = 0, h = maxlen;
+        while(l < h)
+        {
+            int m = l + (h-l)/2;
+            if(dp[m] < v[1])
+                l = m + 1;
+            else h = m;
+        }
+        dp[l] = v[1];
+        if(l == maxlen) maxlen++;
+    }
+    return maxlen;
+}
---
+++ DSA/IP List/Dynamic_Programming/4_Box_Stacking-GFG.cpp
@@ -0,0 +1,26 @@
+
+int maxHeight(int height[],int width[],int length[],int n)
+{
+    vector<vector<int>> box(3*n, vector<int>(3));
+    for(int i = 0; i < n; i++)
+    {
+        box[3*i] = {length[i], min(height[i], width[i]), max(height[i], width[i])};
+        box[3*i + 1] = {width[i], min(height[i], length[i]), max(height[i], length[i])};
+        box[3*i + 2] = {height[i], min(length[i], width[i]), max(length[i], width[i])};
+    }
+    sort(box.begin(), box.end(), [](vector<int> &a, vector<int> &b){
+        return (a[1] == b[1])? (a[2] > b[2]) : (a[1] < b[1]);
+    });
+    
+    vector<int> dp(3*n);
+    int maxans = 0;
+    for(int i = 0; i < 3*n; i++)
+    {
+        dp[i] = box[i][0];
+        for(int j = i-1; j >= 0; j--)
+            if(box[j][2] < box[i][2] && dp[j] + box[i][0] > dp[i])
+                dp[i] = dp[j] + box[i][0];
+        maxans = max(maxans, dp[i]);
+    }
+    return maxans;
+}
