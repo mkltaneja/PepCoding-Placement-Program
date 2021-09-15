7f27991deb17c0ebfa38c2fa10ca815b1a02a3f4
Mukul Taneja <mkltaneja@gmail.com>
2021-09-15 19:44:56 +0530

    BITS - 9

--- DSA/IP List/Bit_Manipulation/8_Minimum Number Of Software Developers-PepCoding.cpp
+++ DSA/IP List/Bit_Manipulation/8_Minimum Number Of Software Developers-PepCoding.cpp
@@ -1,3 +1,6 @@
+
+// METHOD 1 (Recursively)=============================================================
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -46,6 +49,65 @@ int main()
     }
 
     subseq(0, 0, (1 << n) - 1, arr, {});
+
+    cout << "[";
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i];
+        if (i != ans.size() - 1)
+            cout << ", ";
+    }
+    cout << "]";
+}
+
+// METHOD 2 (Iteratively)========================================================
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n;
+    unordered_map<string, int> map;
+    for (int i = 0; i < n; i++)
+    {
+        string x;
+        cin >> x;
+        map[x] = i;
+    }
+    cin >> m;
+    vector<int> arr(m);
+    for (int i = 0; i < m; i++)
+    {
+        int tot;
+        cin >> tot;
+        int fskill = 0;
+        for (int j = 0; j < tot; j++)
+        {
+            string x;
+            cin >> x;
+            fskill |= (1 << map[x]);
+        }
+        arr[i] = fskill;
+    }
+
+    vector<int> ans;
+    
+    for(int x = 0; x < (1 << m); x++)
+    {
+        int totskills = 0;
+        vector<int> tmp;
+        for(int i = 0; i < m; i++)
+        {
+            if(x & (1 << i))
+            {
+                totskills |= arr[i];
+                tmp.push_back(i);
+            }
+        }
+        if(totskills == (1 << n)-1 && (ans.empty() || tmp.size() <= ans.size()))
+            ans = tmp;
+    }
 
     cout << "[";
     for (int i = 0; i < ans.size(); i++)
