97173a82cf7da3db5ee32a802f1c251b15aceb58
Mukul Taneja <mkltaneja@gmail.com>
2021-09-21 15:01:31 +0530

    BITS - 11

---
+++ DSA/IP List/Bit_Manipulation/11_All Repeating Three Times Except One-PepCoding.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int n3 = (1 << 31) - 1, n3p1 = 0, n3p2 = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int tmp0 = (n3 & arr[i]), tmp1 = (n3p1 & arr[i]), tmp2 = (n3p2 & arr[i]);
+        n3 |= tmp2;
+        n3p1 |= tmp0;
+        n3p2 |= tmp1;
+    }
+
+    cout << n3p1 << endl;
+}
